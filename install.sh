a3bafcb12742c69406ac21b9f7b26edb795c3e2c
Kirill Sakharov <mrcaxapov@gmail.com>
2023-01-20 05:59:32 +0300

    Fix install.sh file

--- install.sh
+++ install.sh
@@ -1,13 +1,79 @@
 #!/usr/bin/bash
 
-composer install && npm install
-
-if [ ! -f .env ]; then
-   cp .env.example .env
+if ! command -v php &> /dev/null
+then
+    echo "PHP not installed!"
+    exit
 fi
 
-php artisan key:generate
-php artisan storage:link
+if ! command -v composer &> /dev/null
+then
+    echo "composer not installed!"
+    exit
+fi
 
-echo "Done! Next - config DB connection in .env and enter:"
-echo "php artisan serve and npm run dev"
+if ! command -v node &> /dev/null
+then
+    echo "node js not installed!"
+    exit
+fi
+
+if ! command -v npm &> /dev/null
+then
+    echo "node js not installed!"
+    exit
+fi
+
+if ! command -v mysql &> /dev/null
+then
+    echo "mysql not installed!"
+    exit
+fi
+
+if [ ! -d "./vendor" ]; then
+   composer install
+fi
+
+if [ ! -d "./node_modules" ]; then
+   npm install
+fi
+
+if [ ! -d "./public/storage" ]; then
+   php artisan storage:link
+fi
+
+if [ ! -f "./.env" ]; then
+   cp .env.example .env
+   php artisan key:generate
+   
+   if read -p "Enter your DB user: " user
+   then
+      sed -ir "s/DB_USERNAME=.*/DB_USERNAME=$user/" .env
+   else
+      echo "Done! Next - config DB connection in .env and enter:"
+      echo "php artisan serve and npm run dev"
+      exit
+   fi
+   
+   if read -p "Enter your DB password: " password
+   then
+      sed -ir "s/DB_PASSWORD=.*/DB_PASSWORD=$password/" .env
+   else
+      echo "Done! Next - config DB connection in .env and enter:"
+      echo "php artisan serve and npm run dev"
+      exit
+   fi
+   
+   if read -p "Enter your Database name: " dbname
+   then
+      sed -ir "s/DB_DATABASE=.*/DB_DATABASE=$dbname/" .env
+      mysql -u $user -p$password -e "CREATE DATABASE IF NOT EXISTS $dbname"
+      php artisan migrate
+   else
+      echo "Done! Next - config DB connection in .env and enter:"
+      echo "php artisan serve and npm run dev"
+      exit
+   fi
+fi
+
+echo "Done!"
